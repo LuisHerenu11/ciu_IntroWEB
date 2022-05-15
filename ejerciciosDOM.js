83220f4019f97b5f679982f3f979fe56aaa21cbc
LuisHerenu11 <ElCondor_1106@hotmail.com>
2022-05-15 03:27:49 -0300

    add change

--- ejerciciosDOM.js
+++ ejerciciosDOM.js
@@ -1,13 +1,9 @@
 var primerH1;
 primerH1 = document.querySelector("h1")
 
-
-/*
+/* Ejercicio 1.4
 primerH1.style.height = 300;
 primerH1.style.width = 100;
 primerH1.style.backgroundColor = "red";
-primerH1.style.color = "Red";
 */
-
 primerH1.classList.add('square')
-
