345e3352cff192da87d9d810cd893de8abc261e9
LuisHerenu11 <ElCondor_1106@hotmail.com>
2022-05-18 23:10:46 -0300

    add ejercicio 1.5

--- ejerciciosDOM.js
+++ ejerciciosDOM.js
@@ -7,3 +7,12 @@ primerH1.style.width = 100;
 primerH1.style.backgroundColor = "red";
 */
 primerH1.classList.add('square')
+
+var div5 = document.querySelector("div")
+/* EJERCICIO 1.5 
+    div5.textContent; /* muestra el contenido del elemento como texto
+    div5.textContent="cambio el texto random" /* seteo otro string
+    div5.textContent= "<p> otro parrafo</p>" /*no interpretara la etiqueta <p> ,lo considera un texto
+    div5.innerHTML="<p> inserto un texto con etiqueta usando innerHTML </p>" /* innerHTML si interpreta etiquetas 
+    div5.innerHTML; /* Esto me mostrara el elemento y la etiqueta que lo contiene
+*/
