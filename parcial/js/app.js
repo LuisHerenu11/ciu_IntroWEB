fab1656cea306eae73f0ceccc4d2db14e357130c
LuisHerenu11 <ElCondor_1106@hotmail.com>
2023-05-13 21:50:27 -0300

    agregado modo oscuro y claro

--- parcial/js/app.js
+++ parcial/js/app.js
@@ -1,4 +1,44 @@
-let barra_navegacion = document.getElementsByClassName("iluminado");
+function modo_oscuro(){
+    let cabezal_completo = document.querySelector(".cabezal");
+    cabezal_completo.classList.add("oscuro");
+    let barra_navegacion = document.getElementsByClassName("iluminado");
+    for (element in barra_navegacion){
+        barra_navegacion[element].classList.add("oscuro");
+        barra_navegacion[element].classList.remove("iluminado");
+        barra_navegacion[element].classList.remove("iluminado");
+        /* 
+            tuve que definir doble remove porqué tengo dos veces definido iluminado en objetos super puestos
+            (quería jugar con hover y opacidad entre elementos y no tuve tiempo)
+        */
+    }
+}
 
-barra_navegacion.classList.remove('iluminado');
-barra_navegacion.classList.add('oscuro');
+function modo_claro(){
+    let barra_navegacion = document.getElementsByClassName("oscuro");
+    for (element in barra_navegacion){
+        barra_navegacion[element].classList.add("iluminado");
+        barra_navegacion[element].classList.remove("oscuro");
+        barra_navegacion[element].classList.remove("oscuro");
+    }
+}
+
+function modo_alternado(){
+    let cabezal_completo = document.querySelector(".cabezal");
+    if(cabezal_completo.classList.contains("oscuro")){
+        cabezal_completo.classList.toggle("oscuro");
+    }else{
+        if(cabezal_completo.classList.contains("iluminado")){
+            cabezal_completo.classList.toggle("iluminado");
+        }
+        /*
+            pendiente por falta de tiempo!
+        if(cabezal_completo.style.background.color == "E400FF"){
+            cabezal_completo.setAttribute.style.background.color = "black";
+        }
+        */
+    }
+    let barra_navegacion = document.getElementsByClassName("oscuro");
+    for (element in barra_navegacion){
+        barra_navegacion[element].classList.toggle("oscuro");
+    }
+}
